f3bbb681216bd15f549ffc219eed405f665a14fc
best-lucky <bestlucky1030@gmail.com>
2021-10-19 08:09:14 +0300

    update package and menu component.

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,16 @@
 </template>
 
 <script>
-import Menu from './components/Menu.vue'
+import Menu from '@/components/Menu.vue'
 
 export default {
   name: 'App',
   components: {
-    Menu
+    Menu,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
 </style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,7 +1,24 @@
 <template>
   <div id="menu">
-    <v-toolbar color="green" dark max-height="64">
+    <v-toolbar color="purple" dark max-height="64">
       <v-toolbar-title>Soccer League</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text class="d-none d-sm-flex">
+          <router-link class="white--text" to="/">Home</router-link>
+        </v-btn>
+        <v-btn text class="d-none d-sm-flex">
+          <router-link class="white--text" to="/live">Live</router-link>
+        </v-btn>
+        <v-btn text class="d-none d-sm-flex">
+          <router-link class="white--text" to="/myteams">My Teams</router-link>
+        </v-btn>
+        <v-btn icon @click.stop="drawer = !drawer" class="d-flex d-sm-none">
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
   </div>
 </template>
@@ -9,5 +26,10 @@
 <script>
   export default {
     name: 'Menu',
+    data() {
+      return {
+        drawer: false,
+      }
+    }
   }
 </script>
