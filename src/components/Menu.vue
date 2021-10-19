7c33e72fe90fdb34e0d798898b2c9f31640f6f26
best-lucky <bestlucky1030@gmail.com>
2021-10-19 05:28:49 +0300

    add some packages and files.

---
+++ src/components/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="menu">
+    <v-toolbar color="green" dark max-height="64">
+      <v-toolbar-title>Soccer League</v-toolbar-title>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Menu',
+  }
+</script>
