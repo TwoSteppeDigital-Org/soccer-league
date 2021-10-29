3b178a5a600daf32dbf575547c8e8d53808a2351
best-lucky <bestlucky1030@gmail.com>
2021-10-29 13:49:14 +0300

    Initial dist subtree commit

---
+++ precache-manifest.049bbad63f94535a0635b1503f7ce912.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9114295549cc3d92e93",
+    "url": "/soccer-league/css/app.fb15b1a1.css"
+  },
+  {
+    "revision": "d9114295549cc3d92e93",
+    "url": "/soccer-league/js/app.4cbc1cbd.js"
+  },
+  {
+    "revision": "c195e60ead84349f2a22",
+    "url": "/soccer-league/css/chunk-vendors.986aa9ab.css"
+  },
+  {
+    "revision": "c195e60ead84349f2a22",
+    "url": "/soccer-league/js/chunk-vendors.595b2482.js"
+  },
+  {
+    "revision": "90d4dee730fdeb32dc6810df3584990b",
+    "url": "/soccer-league/index.html"
+  }
+];
